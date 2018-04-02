fe2d1f43e4b433f87d5840b6db1b8ce2e209b56b
Andrew Thompson <andrew@hijacked.us>
2018-04-02 13:37:32 -0700

    Fix dealer management, improve generator again

--- eqc/combine_signature_shares_eqc.erl
+++ eqc/combine_signature_shares_eqc.erl
@@ -7,13 +7,14 @@
 prop_combine_signature_shares() ->
     ?FORALL({{Players, Threshold}, Curve}, {gen_players_threshold(), gen_curve()},
             begin
-                dealer:start_link(Players, Threshold, Curve),
+                {ok, _} = dealer:start_link(Players, Threshold, Curve),
                 {ok, K} = dealer:adversaries(),
                 {ok, _Group} = dealer:group(),
                 {ok, _G1, G2, PubKey, PrivateKeys} = dealer:deal(),
                 MessageToSign = tpke_pubkey:hash_message(PubKey, crypto:hash(sha256, crypto:strong_rand_bytes(12))),
                 Signatures = [ tpke_privkey:sign(PrivKey, MessageToSign) || PrivKey <- PrivateKeys],
                 Sig = tpke_pubkey:combine_signature_shares(PubKey, dealer:random_n(K, Signatures)),
+                gen_server:stop(dealer),
                 ?WHENFAIL(begin
                               io:format("Signatures ~p~n", [[ erlang_pbc:element_to_string(S) || {_, S} <- Signatures]])
                           end,
@@ -27,7 +28,7 @@ gen_players_threshold() ->
     ?SUCHTHAT({Players, Threshold},
               ?LET({X, Y},
                    ?SUCHTHAT({A, B}, {int(), int()}, A > 0 andalso B > 0 andalso A > B),
-                   {X, (X div 4)+Y}),
+                   {X*3, X - Y}),
               Players > 3*Threshold+1).
 
 gen_curve() ->
