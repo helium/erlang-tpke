4a4d5a9a931180dd289c20f8d5057042344546c2
Andrew Thompson <andrew@hijacked.us>
2020-04-20 15:11:02 -0700

    Optimize threshold decryption code
    In the Baek and Zhang paper the decrypt_share step can return a valid
    share or a `?` to indicate that the ciphertext was not consistent with
    the private key. This `?` share was propogated forwards through the
    protocol and finally filtered out in the combine_shares step.
    
    These changes reduce the redundant verification done for decryption
    shares when generating/validating and combining them. Instead this code
    tracks the validation state of the ciphertext and throws exceptions if a
    non validated ciphertext is used in any context other than
    validate_ciphertext.

--- src/tpke_privkey.erl
+++ src/tpke_privkey.erl
@@ -14,7 +14,7 @@
 
 -opaque privkey() :: #privkey{}.
 -opaque privkey_serialized() :: #privkey_serialized{}.
--type share() :: {non_neg_integer(), erlang_pbc:element() | '?'}.
+-type share() :: {non_neg_integer(), erlang_pbc:element()}.
 
 -export_type([privkey/0, share/0, privkey_serialized/0]).
 
@@ -26,18 +26,13 @@ init(PubKey, SecretKey, SecretKeyIndex) ->
 
 %% Section 3.2.2 Baek and Zheng
 %% Dski(C):
--spec decrypt_share(privkey(), {erlang_pbc:element(), binary(), erlang_pbc:element()}) -> share().
-decrypt_share(PrivKey, {U, V, W}) ->
-    Share = case tpke_pubkey:verify_ciphertext(PrivKey#privkey.pubkey, {U, V, W}) of
-                true ->
-                    %% computes Ui = xiU
-                    erlang_pbc:element_mul(PrivKey#privkey.secret_key, U);
-                false ->
-                    '?'
-            end,
+-spec decrypt_share(privkey(), tpke_pubkey:ciphertext()) -> share().
+decrypt_share(PrivKey, CipherText) ->
+    {U, _V, _W} = tpke_pubkey:check_ciphertext(PrivKey#privkey.pubkey, CipherText),
+    %% computes Ui = xiU
+    Share = erlang_pbc:element_mul(PrivKey#privkey.secret_key, U),
     %% output Di = (i, Ui)
     {PrivKey#privkey.secret_key_index, Share}.
-
 
 %% Section 5.2 Boldyrevya
 %% MS
--- test/decrypt_SUITE.erl
+++ test/decrypt_SUITE.erl
@@ -22,14 +22,29 @@ threshold_decrypt_test(Config) ->
     lists:foreach(fun({ok, Dealer}) ->
                           {ok, _Group} = dealer:group(Dealer),
                           {ok, {PubKey, PrivateKeys}} = dealer:deal(Dealer),
+                          {ok, {OtherPubKey, OtherPrivateKeys}} = dealer:deal(Dealer),
                           {ok, K} = dealer:threshold(Dealer),
                           Message = crypto:hash(sha256, <<"my hovercraft is full of eels">>),
                           CipherText = tpke_pubkey:encrypt(PubKey, Message),
                           %% verify ciphertext
-                          ?assert(tpke_pubkey:verify_ciphertext(PubKey, CipherText)),
-                          Shares = [ tpke_privkey:decrypt_share(SK, CipherText) || SK <- PrivateKeys ],
+                          ?assertError(unverified_ciphertext, [ tpke_privkey:decrypt_share(SK, CipherText) || SK <- PrivateKeys ]),
+                          {ok, VerifiedCipherText} = tpke_pubkey:verify_ciphertext(PubKey, CipherText),
+                          ?assertError(inconsistent_ciphertext, [ tpke_privkey:decrypt_share(SK, VerifiedCipherText) || SK <- OtherPrivateKeys ]),
+                          Shares = [ tpke_privkey:decrypt_share(SK, VerifiedCipherText) || SK <- PrivateKeys ],
                           %% verify share
-                          ?assert(lists:all(fun(X) -> X end, [tpke_pubkey:verify_share(PubKey, Share, CipherText) || Share <- Shares])),
+                          ?assertError(unverified_ciphertext, lists:all(fun(X) -> X end, [tpke_pubkey:verify_share(PubKey, Share, CipherText) || Share <- Shares])),
+                          ?assertError(inconsistent_ciphertext, lists:all(fun(X) -> X end, [tpke_pubkey:verify_share(OtherPubKey, Share, VerifiedCipherText) || Share <- Shares])),
+                          ?assert(lists:all(fun(X) -> X end, [tpke_pubkey:verify_share(PubKey, Share, VerifiedCipherText) || Share <- Shares])),
                           %% verify combine_shares
-                          ?assertEqual(Message, tpke_pubkey:combine_shares(PubKey, CipherText, dealer:random_n(K, Shares)))
+                          ?assertError(unverified_ciphertext, tpke_pubkey:combine_shares(PubKey, CipherText, dealer:random_n(K, Shares))),
+                          ?assertError(inconsistent_ciphertext, tpke_pubkey:combine_shares(OtherPubKey, VerifiedCipherText, dealer:random_n(K, Shares))),
+                          ?assertEqual(Message, tpke_pubkey:combine_shares(PubKey, VerifiedCipherText, dealer:random_n(K, Shares))),
+                          %% test serialization/deserialization
+                          SerializedCipherText = tpke_pubkey:ciphertext_to_binary(VerifiedCipherText),
+                          %% we don't encode validation status
+                          ?assertEqual(SerializedCipherText, tpke_pubkey:ciphertext_to_binary(CipherText)),
+                          DeserializedCipherText = tpke_pubkey:binary_to_ciphertext(SerializedCipherText, PubKey),
+                          ?assertError(inconsistent_ciphertext, tpke_pubkey:binary_to_ciphertext(SerializedCipherText, OtherPubKey)),
+                          ?assertEqual(Message, tpke_pubkey:combine_shares(PubKey, DeserializedCipherText, dealer:random_n(K, Shares))),
+                          ok
                   end, Dealers).
